93ba1b623b4e72dc3377a41bb6a50f3d79bb66d5
ritish dhiman <46323013+ritish26@users.noreply.github.com>
2020-06-27 09:38:35 +0530

    Add files via upload

---
+++ Divide and Conquer/Sort_012.cpp
@@ -0,0 +1,19 @@
+void sort012(int arr[], int n)
+{
+    int l=0,m=0,h=n-1;
+    while(m<=h)
+    {
+        if(arr[m]==0)
+        {
+            swap(arr[m],arr[l]); l++; m++;
+        }
+        else if(arr[m]==1)
+        m++;
+        else
+        {
+            swap(arr[m],arr[h]);
+            h--;
+        }
+        
+    }
+}
