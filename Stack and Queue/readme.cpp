ca8fce3719e5dce69f3d23f88fc192ad199c2b5c
Ritish <ritish.dhiman26@gmail.com>
2025-05-21 00:46:00 +0530

    updated readme

---
+++ Stack and Queue/readme.cpp
@@ -0,0 +1,14 @@
+stack<T> s;                   // Create a stack of type T
+s.push(val);                  // Add element on top
+s.pop();                      // Remove top element
+s.top();                      // Access top element
+s.empty();                    // Check if stack is empty
+s.size();                     // Get size of the stack
+
+queue<T> q;                   // Create a queue of type T
+q.push(val);                  // Add element at the back
+q.pop();                      // Remove element from the front
+q.front();                    // Access front element
+q.back();                     // Access last (back) element
+q.empty();                    // Check if queue is empty
+q.size();                     // Get size of the queue
